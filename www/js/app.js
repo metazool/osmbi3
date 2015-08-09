4878463d9eda243aad3979665bc48438d9bf041e
Jo Walsh <metazool@fastmail.net>
2015-08-09 12:04:08 +0100

    working demo version

--- www/js/app.js
+++ www/js/app.js
@@ -18,7 +18,7 @@ angular.module('osmbi', ['ionic','ngCordova'])
   });
 })
 
-.controller('mapController',function($scope,$http,$cordovaGeolocation,$cordovaNetwork,$cordovaOAuth,overpassAPI) {
+.controller('mapController',function($scope,$http,$cordovaGeolocation,$cordovaNetwork,$cordovaOauth,overpassAPI) {
 
 //.controller('mapController',function($scope,$http,overpassAPI) {
 
@@ -28,22 +28,17 @@ angular.module('osmbi', ['ionic','ngCordova'])
 
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+		   }).addTo(map);
 	map.setView([x, y], zoom);
-	overpassAPI.search('[bbox][out:json];way[building=yes];out;&bbox='+map.getBounds().toBBoxString()).then(function(data) {
-		console.log(data);
-	},
-	function(err) {
-		console.log(err);
-	});
    } 
-   initMap(51.505,-0.09, 20);
+   initMap(51.5237278,-0.1048208, 20);
 
    var watchOptions = {
     frequency : 1000,
     timeout : 3000,
     enableHighAccuracy: false // may cause errors if true
   };
+
 
   var watch = $cordovaGeolocation.watchPosition(watchOptions);
   watch.then(
@@ -56,9 +51,37 @@ angular.module('osmbi', ['ionic','ngCordova'])
       $scope.lat  = position.coords.latitude
       $scope.long = position.coords.longitude
       map.setView([$scope.lat, $scope.long],20);
-  });
+	   overpassAPI.search('[bbox][out:json];way;out center;&bbox='+map.getBounds().toBBoxString()).then(function(data) {
+            var places = data.data.elements;
+            for (i = 0, len = places.length; i < len; i++) {
+                p = places[i];
+                lat = p.center.lat;
+                long = p.center.lon;
+                tags = p.tags;
+                if (tags && tags.hasOwnProperty("building")) {
 
-  
+                var myIcon = L.divIcon(
+                                {className: 'my-div-icon',
+                                 html:"yes!"});
+                L.marker([lat, long], {icon: myIcon}).addTo(map);                   
+                }
+                }
+        },
+        function(err) {
+                console.log(err);
+        });
+	
+     });
+
+    $scope.login = function() {
+	var client_id = 'c27e3a1d9eaed5ff7f95'
+	var client_secret = '9c9d23dd46749808b5a1e93ba1b8ded37eddc463'
+        $cordovaOauth.github(client_id,client_secret).then(function(result) {
+            console.log(JSON.stringify(result));
+        }, function(error) {
+            console.log(error);
+        });
+    }  
   //var t = $cordovaNetwork.getNetwork();
 })
 
