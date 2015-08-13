21dee9d0f7712707b14958bb05887588464dc146
Jo Walsh <metazool@fastmail.net>
2015-08-13 03:26:17 +0100

    backed out of the auth tactic, didnt work on mobile

--- www/js/app.js
+++ www/js/app.js
@@ -62,6 +62,9 @@ angular.module('osmbi', ['ionic','ngCordova'])
   };
 
     $scope.heights = {};  
+    $scope.augment = function(way_id) {
+	scope.heights[way_id] +=1;
+    }
     var watch = $cordovaGeolocation.watchPosition(watchOptions);
     watch.then(
        null,
@@ -85,21 +88,24 @@ angular.module('osmbi', ['ionic','ngCordova'])
                 tags = p.tags;
                 if (tags && tags.hasOwnProperty("building")) {
 		    if (tags.hasOwnProperty("building:levels")) {
-			html = tags['building:levels'];
+			$scope.heights[p.id] = tags['building:levels'];
 	            }		
-
+		    
                     var myIcon = L.divIcon(
-                                {className: p.id,
-                                 html:'<div id="'+p.id+'">'+$scope.heights[p.id]+'</div>'});
+                                {className: 'my-div-icon',
+				 iconSize: [30,30],
+                                 html:'<div ontouchstart="augment('+p.id+')"on-touch="augment('+p.id+')" id="'+p.id+'">'+$scope.heights[p.id]+'</div>'});
 	            
                     var marker = L.marker([lat, long], {icon: myIcon}).addTo(map)
 		    .on('click',function(e) {
 			var way_id = e.originalEvent.srcElement.id;
+			console.log(way_id);
 			$scope.heights[way_id] +=1;
 			console.log($scope.heights[way_id]);
 			// leaflet got in angular's way :/
 			var newIcon = L.divIcon( 
-				{className: p.id,
+				{className: 'my-div-icon',
+				 iconSize:[30,30],
                                  html:'<div id="'+way_id+'">'+$scope.heights[way_id]+'</div>'});
 			e.target.setIcon(newIcon);
 		    });
@@ -119,21 +125,19 @@ angular.module('osmbi', ['ionic','ngCordova'])
     var auth = osmAuth({
         oauth_consumer_key: 'RhPXthrBJX2kpRhj9h9LZrrrMgb38m4KMQ2vtXaX',
         oauth_secret: '7qAeSPIcF6UHtoO6IaR9dKd9qcmtNRq2h7kuJGeG',
-        auto: true // show a login form if the user is not authenticated and
-                // you try to do a call
+ 	singlepage: true,
+	auto: true
    });
 
-    $scope.login = function() {
-    	auth.xhr({
-            method: 'GET',
-            path: '/api/0.6/user/details'
-    	}, function(err, details) {
-            // details is an XML DOM of user details
-	    console.log(details);
-	    $scope.authed = 1;
-    	});
-    };
-
+   $scope.login = function() {
+	var client_id = 'c27e3a1d9eaed5ff7f95'
+	var client_secret = '9c9d23dd46749808b5a1e93ba1b8ded37eddc463'
+        $cordovaOauth.openstreetmap(client_id,client_secret).then(function(result) {
+            console.log(JSON.stringify(result));
+        }, function(error) {
+            console.log(error);
+        });
+    }  
 })
 
 .factory('overpassAPI', function($http) {
